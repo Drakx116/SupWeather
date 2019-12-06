caa731b188d4c79c2cfd02b552c12c4756153ec3
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-07 00:26:13 +0100

    MongoDB Cluster Database access setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const ENV = require('dotenv').config().parsed;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+mongoose.connect(ENV.ATLAS_DATABASE_CONNECTION_PATH, { useNewUrlParser: true }).catch(
+    () => console.log('An error has occurred while connecting the database.')
+);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.route('/')
     .get((req, res) => {
