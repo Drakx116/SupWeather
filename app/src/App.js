a82efd73e5d1496481c3cf89b5ced25b39884296
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-10 09:45:01 +0100

    Init router

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
-      <div>
-        <h1> SupWeather </h1>
-      </div>
+    <Router>
+        <div className="header">
+            <h2> SupWeather </h2>
+            <ul>
+                <li><Link to="/"> Dashboard </Link></li>
+                <li><Link to="/users"> Users </Link></li>
+            </ul>
+        </div>
+
+        <Switch>
+            <Route exact path="/">
+                Dashboard
+            </Route>
+            <Route exact path="/users">
+                Users
+            </Route>
+        </Switch>
+    </Router>
   );
 }
 
