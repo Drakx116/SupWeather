668d331063f9e6da862c69eae1606e17645a2e31
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-20 21:15:51 +0100

    Basic login form Component

--- app/src/Components/Security/Login/Login.js
+++ app/src/Components/Security/Login/Login.js
@@ -1,12 +1,36 @@
 import React from "react";
 
 export class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Le nom a été soumis : ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="component-container">
                 <h2> Welcome back ! </h2>
 
-                {/* TODO : Add login form / https://fr.reactjs.org/docs/forms.html */}
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Pseudo :
+                        <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={"Pseudo"} />
+                    </label>
+                    <input type="submit" value="Envoyer" />
+                </form>
             </div>
         );
     }
