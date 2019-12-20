99ee8b79d48e4ab08932c6c4f535f665bdd50a64
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-21 00:42:22 +0100

    Fetch POST /login on API

--- app/src/Components/Security/Login/Login.js
+++ app/src/Components/Security/Login/Login.js
@@ -4,19 +4,56 @@ export class Login extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {value: ''};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            pseudo: undefined,
+            password: undefined,
+            invalidForm: ''
+        };
+
+        this.changePseudo = this.changePseudo.bind(this);
+        this.changePassword = this.changePassword.bind(this);
+        this.submitForm = this.submitForm.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    // Input handlers
+    changePseudo(event) {
+        this.setState({ pseudo: event.target.value });
     }
 
-    handleSubmit(event) {
-        alert('Le nom a été soumis : ' + this.state.value);
+    changePassword(event) {
+        this.setState({ password: event.target.value });
+    }
+
+    // Form and API call
+    submitForm(event)  {
+        // Avoids page refreshment
         event.preventDefault();
+
+        if(this.state.pseudo && this.state.password)
+        {
+            let data = {
+                pseudo: this.state.pseudo,
+                password: this.state.password
+            };
+
+            fetch('http://localhost:3000/auth/login', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }}
+            )
+                .then(
+                    res => res.redirect('/')
+                )
+                .catch(
+                    error => console.error('Error', error)
+                );
+        }
+        else {
+            this.setState({ invalidForm: 'Empty fields.' });
+        }
     }
 
     render() {
@@ -24,13 +61,23 @@ export class Login extends React.Component {
             <div className="component-container">
                 <h2> Welcome back ! </h2>
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.submitForm}>
                     <label>
-                        Pseudo :
-                        <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={"Pseudo"} />
+                        Pseudo :  <br />
+                        <input type="text" value={this.state.pseudo} onChange={this.changePseudo} />
                     </label>
+                    <br/>
+
+                    <label>
+                        Mot de passe :  <br />
+                        <input type="password" value={this.state.password} onChange={this.changePassword} />
+                    </label>
+
+                    <br /><br/>
                     <input type="submit" value="Envoyer" />
                 </form>
+                <br/>
+                <b> { this.state.invalidForm } </b>
             </div>
         );
     }
