bd179282a39f381b91df9d747803e906c0ec583f
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-21 00:49:40 +0100

    Spacing

--- server/src/controllers/dashboard.js
+++ server/src/controllers/dashboard.js
@@ -1,7 +1,7 @@
 const express = require('express');
 export const DashboardRoutes = express.Router();
 
-
 DashboardRoutes
-    .get('/', (req, res) => res.status(200).json({ data: 'SupWeather API is Alive !' }));
-
+    .get('/', (req, res) => res.status(200).json({
+        data: 'SupWeather API is Alive !'
+    }));
