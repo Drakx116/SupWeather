483d229bf9e03b9206afe5855dac9e525df3b51c
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-21 13:38:07 +0100

    Removed auth message on dashboard

--- app/src/Components/Dashboard/Dashboard.js
+++ app/src/Components/Dashboard/Dashboard.js
@@ -1,16 +1,10 @@
 import React from "react";
-import cookie from "react-cookies";
 
 export class Dashboard extends React.Component {
     render() {
-
-        let token = cookie.load('token');
-        let message = (token) ? 'Auth with token : ' + token : 'Guest';
-
         return (
             <div className="component-container">
                 <h2> Dashboard </h2>
-                <p> { message }</p>
             </div>
         );
     }
