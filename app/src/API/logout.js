68c805f5b51dac764ce712933ece877ef2af072d
Drakx116 <alexandre2.guyot@gmail.com>
2019-12-30 14:10:35 +0100

    Working client /logout and fix redirections issues

--- app/src/API/logout.js
+++ app/src/API/logout.js
@@ -1,1 +1,22 @@
 // Fetch /auth/logout, check tokens
+import cookie from "react-cookies";
+
+export const logout = (data, props) => {
+    fetch('http://localhost:3000/auth/logout',
+        {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(() => {
+            cookie.remove('token');
+
+            // Redirect
+            window.location.reload();
+        })
+
+        .catch( error => console.error('Error', error) );
+};
---
+++ app/src/Components/Security/Logout.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import {Redirect, withRouter} from "react-router-dom";
+import cookie from 'react-cookies';
+import { logout } from "../../API/logout";
+import {Dashboard} from "../Dashboard/Dashboard";
+
+class Logout extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        const token = cookie.load('token');
+        logout(
+            {
+                token: token
+            },
+            this.props
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <Redirect to={Dashboard} />
+            </div>
+        );
+    }
+}
+
+export default withRouter(Logout);
