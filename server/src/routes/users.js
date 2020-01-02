4a2953257c90cd345de3e57b06b0b8066e402f90
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-02 03:16:53 +0100

    Add WeatherAPI token

--- server/src/routes/users.js
+++
@@ -1,7 +0,0 @@
-import { needAuthentication } from "../controllers/security/access"
-import { getUsers } from "../controllers/users";
-
-export const UserRoutes = require('express').Router();
-
-UserRoutes
-    .get('/', needAuthentication, getUsers);
