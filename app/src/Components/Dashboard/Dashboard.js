a928511fb029b7a9c6681b83d29c8c5bcbb20b18
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-02 14:40:32 +0100

    Fix auth redirections

--- app/src/Components/Dashboard/Dashboard.js
+++ app/src/Components/Dashboard/Dashboard.js
@@ -1,7 +1,11 @@
 import React from "react";
+import cookie from "react-cookies";
 
 export class Dashboard extends React.Component {
     render() {
+        if(!cookie.load('token')) {
+            this.props.history.push('/login');
+        }
         return (
             <div className="component-container">
                 <h2> Dashboard </h2>
