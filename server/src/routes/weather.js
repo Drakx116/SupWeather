fda5c5ebbd49e40b7fc1772efa57b693c279038a
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-05 20:48:49 +0100

    Add user GET /weather/city/list route

--- server/src/routes/weather.js
+++ server/src/routes/weather.js
@@ -1,7 +1,10 @@
 import { getCityWeather } from "../controllers/weather";
+import { getUserCityList } from "../controllers/city";
 import {needAuthentication} from "../controllers/security/access";
 
 export const WeatherRoutes = require('express').Router();
 
 WeatherRoutes
+    .get('/city/list', needAuthentication, getUserCityList)
+
     .get('/city/:city', needAuthentication, getCityWeather);
