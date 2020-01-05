29f659eb1854e9ea5e3be6f84648b66687eb850d
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-05 17:30:59 +0100

    Add auth verification on GET /weather/city/:city

--- server/src/routes/weather.js
+++ server/src/routes/weather.js
@@ -1,6 +1,7 @@
 import { getCityWeather } from "../controllers/weather";
+import {needAuthentication} from "../controllers/security/access";
 
 export const WeatherRoutes = require('express').Router();
 
 WeatherRoutes
-    .get('/city/:city', getCityWeather);
+    .get('/city/:city', needAuthentication, getCityWeather);
