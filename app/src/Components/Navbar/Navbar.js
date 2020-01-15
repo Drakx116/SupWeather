51776e6bb25218066a8e49e6679751776c3dfcf1
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-15 14:51:47 +0100

    Server side token validaties components rendering

--- app/src/Components/Navbar/Navbar.js
+++ app/src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import cookie from 'react-cookies';
 
 import { AuthNavbar } from "./templates/AuthNavbar";
 import { GuestNavbar } from "./templates/GuestNavbar";
@@ -8,7 +9,18 @@ export class Navbar extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { isAuth: this.props.isAuth };
+        this.state = { isAuth: false };
+
+        fetch('http://localhost:3000/auth/checkToken', {
+            method: 'GET',
+            headers: {
+                'Authorization': cookie.load('token')
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ isAuth: !!(data.data) })
+            })
     }
 
 
