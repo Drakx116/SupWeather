a9a9a2e0caa0adc16064e3e8ee81a535806e4126
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-16 07:48:35 +0100

    Marlers style

--- app/src/Components/GoogleMap/Marker/Marker.js
+++ app/src/Components/GoogleMap/Marker/Marker.js
@@ -7,17 +7,24 @@ export class Marker extends React.Component {
         super(props);
 
         this.state = {
-            label: this.props.text
-        }
+            url : "/city/" + this.props.text.toLowerCase()
+        };
     }
 
     render() {
-        const url = `/city/` + this.state.label.toLowerCase();
         return (
-            <a href={url}>
+            <a href={this.state.url}>
                 <div className='google-maps-marker'>
-                    <div className="marker-text">
-                        { this.state.label }
+                    <div className="marker-info">
+                        <div className="marker-info-city">
+                            { this.props.text }
+                        </div>
+                        <div className="marker-info-value">
+                            Temperature Actuelle : 4.5°
+                        </div>
+                        <div className="marker-info-value">
+
+                        </div>
                     </div>
                 </div>
             </a>
